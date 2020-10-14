611b5d7515ac72d100d87c5c4623ff5f5fe6a397
Giang coi <thanhgianga303@gmail.com>
2020-10-15 00:23:12 +0700

    test encoding

--- Client/src/client/Client.java
+++ Client/src/client/Client.java
@@ -25,18 +25,19 @@ public class Client {
 
 	public Client(String address, int port) throws UnknownHostException, IOException
 	{ 
+                System.setProperty("file.encoding", "UTF-8");
 		socket = new Socket(address, port); 
 		System.out.println("Connected"); 
 		out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF8"));
 		in = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF8"));
 		stdIn = new BufferedReader(new InputStreamReader(System.in,"iso-8859-1"));
-
+                    
 		String line = ""; 
-		while (!line.equals("Over")) 
+		while (!line.equals("bye")) 
 		{ 
 			line = stdIn.readLine();
 			System.out.println("Client sent: " + line);
-			out.write(line);
+			out.write(line.toLowerCase());
 			out.newLine();
 			out.flush();
                         line = in.readLine();
